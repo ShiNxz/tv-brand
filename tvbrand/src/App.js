b2f9a7d614f9c5ce5a4eee74ab87987874a97003
Amir Eliz <Amir.e@next-il.co.il>
2022-03-22 08:49:17 +0200

    new

---
+++ tvbrand/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/UI/Navbar';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+    <div className="App">
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
